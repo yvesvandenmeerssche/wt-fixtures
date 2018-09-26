0856c5cecde2e3d207a7201c62f127ade9d5da71
Jirka Chadima <chadima.jiri@gmail.com>
2018-09-26 15:03:09 +0200

    feat: Adds more time-based data to regenerate-availability

--- scripts/regenerate-availability.js
+++ scripts/regenerate-availability.js
@@ -10,15 +10,29 @@ function log (msg) {
 
 function main () {
   const hotelPath = process.argv[2];
-  if (!hotelPath) {
-    throw new Error('Usage: node regenerate-availability.js <path-to-hotel-dir>');
+  const what = process.argv[3];
+  if (!hotelPath || !what ||
+    (what !== 'availability' && what != 'cancellationPolicies' && what !== 'ratePlans')
+    ) {
+    throw new Error('Usage: node regenerate.js <path-to-hotel-dir> [availability|cancellationPolicies|ratePlans] ');
   }
-  log(`Updating availability definition at ${hotelPath}`);
+  log(`Updating ${what} definition at ${hotelPath}`);
   const hotelData = require(path.join(hotelPath, 'definition.json'));
   if (!hotelData) {
-    throw new Error(`${hotelPath} does contains broken definition.json`);
+    throw new Error(`${hotelPath} contains broken definition.json`);
   }
-  hotelData.availability = generators.generateAvailability(hotelData.description);
+  switch (what) {
+    case 'availability':
+      hotelData.availability = generators.generateAvailability(hotelData.description);
+      break;
+    case 'cancellationPolicies':
+      hotelData.description.cancellationPolicies = generators.generateCancellationPolicies();
+      break;
+    case 'ratePlans':
+      hotelData.ratePlans = generators.generateRatePlans(hotelData.description);
+      break;
+  }
+  
   fs.writeFileSync(path.join(hotelPath, 'definition.json'),
       JSON.stringify(hotelData, null, '  '));
   log('Done.');
